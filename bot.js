3d2b9c6d691c35fca50a32ed08f76dc0bc8de158
hamo21 <40078281+hamo21@users.noreply.github.com>
2018-06-19 04:28:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,17 +20,7 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === 'رابط') {
-    msg.reply('** ``https://discord.gg/YgEyRQY``**');
-  }
-});
 
-client.on('message', msg => {
-  if (msg.content === 'الرابط') {
-    msg.reply('** ``https://discord.gg/YgEyRQY``**');
-  }
-});
 
 client.on('message', msg => {
   if (msg.content === 'باك') {
@@ -144,7 +134,7 @@ client.on('guildMemberAdd', member => {
     .setColor('GREEN')
     .setFooter('==== WELCOME TO BLACK MARKET ====', 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
 
-var channel =member.guild.channels.find('name', '𝗖𝗛𝗔𝗧')
+var channel =member.guild.channels.find('name', 'chat')
 if (!channel) return;
 channel.send({embed : embed});
 });
@@ -159,13 +149,30 @@ client.on('guildMemberRemove', member => {
     .setColor('RED')
     .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
 
-var channel =member.guild.channels.find('name', '𝗖𝗛𝗔𝗧')
+var channel =member.guild.channels.find('name', 'chat')
 if (!channel) return;
 channel.send({embed : embed});
 })
 
 
-var prefix = "!";
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+
+var prefix = "@";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -180,6 +187,123 @@ client.on("message", message => {
 };     
 });
 
+client.on('message', msg => {
+  if (msg.content === 'رابط') {
+    msg.reply('**تم ارسال الرابط في الخاص**');
+  }
+});
+
+    
+client.on('message', message => {
+     if (message.content === "رابط") {
+message.author.send("***WELCOME TO ZOMBIE***" + `  **
+https://discord.gg/sDnMWNT
+**`);
+    }
+});      
+
+
+client.on("message", message => {
+  var prefix = "#";
+
+          var args = message.content.substring(prefix.length).split(" ");
+          if (message.content.startsWith(prefix + "مسح")) {
+ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+      var msg;
+      msg = parseInt();
+    
+    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+    message.channel.sendMessage("", {embed: {
+      title: "Done | تــم",
+      color: 0x06DF00,
+      description: "تم مسح الرسائل بنجاح",
+      footer: {
+        text: "ZOMBIE Bot."
+      }
+    }}).then(msg => {msg.delete(3000)});
+                        }
+
+   
+});
+
+
+client.on('message', eyad => {
+  if (eyad.content.startsWith('#اسكت')) {
+if (!eyad.member.hasPermission("MOVE_MEMBERS")) return eyad.channel.send("**انت لا تمتلك الخاصيه المطلوبه** | ❎ ");
+let men = eyad.mentions.users.first()
+let mas = eyad.author
+if(!men) return eyad.channel.send('`منشن الشخص الذي تريد ان تعطيه ميوت كتابي` ');
+eyad.guild.channels.forEach(c => {
+c.overwritePermissions(men.id, {
+          SEND_MESSAGES: false
+})
+    })
+const embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setDescription(`**
+ <@${men.id}>
+لقد تم اعطائك ميوت كتابي
+بواسطة : <@${eyad.author.id}> **`)
+.setThumbnail("https://cdn.discordapp.com/attachments/408952032112803850/452090205793681419/fd684707fc14f41663f15ecebf089f06.png")
+          
+client.users.get(men.id).sendEmbed(embed)
+const Embed11 = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setAuthor(eyad.guild.name, eyad.guild.iconURL)
+.setDescription(`          <@${men.id}>
+لقد تم اعطائه الميوت الكتابي بنجاح
+بواسطة : <@${eyad.author.id}> `)
+.setThumbnail("https://cdn.discordapp.com/attachments/408952032112803850/452090205793681419/fd684707fc14f41663f15ecebf089f06.png")
+eyad.channel.sendEmbed(Embed11).then(eyad => {eyad.delete(20000)})
+    }
+})
+
+
+client.on('message', eyad => {
+  if (eyad.content.startsWith('#تكلم')) {
+if (!eyad.member.hasPermission("MOVE_MEMBERS")) return eyad.channel.send("**انت لا تمتلك الخاصيه المطلوبه** | ❎ ");
+ let men = eyad.mentions.users.first()
+ let mas = eyad.author
+ if(!men) return eyad.channel.send('`منشن الشخص الذي تريد فك الميوت عنه `');
+ eyad.guild.channels.forEach(c => {
+ c.overwritePermissions(men.id, {
+         SEND_MESSAGES: true
+         })
+    })
+const embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setDescription(`**
+ <@${men.id}>
+تم فك الميوت الكتابي 
+بواسطة : <@${eyad.author.id}> **`)
+.setThumbnail("https://cdn.discordapp.com/attachments/408952032112803850/452093541003296788/start-button-hi.png")
+          
+client.users.get(men.id).sendEmbed(embed)
+const Embed11 = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setAuthor(eyad.guild.name, eyad.guild.iconURL)
+.setDescription(`          <@${men.id}>
+تم فك الميوت الكتابي 
+بواسطة : <@${eyad.author.id}>
+`)
+.setThumbnail("https://cdn.discordapp.com/attachments/408952032112803850/452093541003296788/start-button-hi.png")
+eyad.channel.sendEmbed(Embed11).then(eyad => {eyad.delete(20000)})
+    }
+})
+
+var prefix = "$";
+client.on("message", message => {
+    if(message.content.startsWith(prefix + 'vc')) {
+     let args = message.content.split(" ").slice(1);
+       var nam = args.join(' ');
+    
+      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`manager Roles` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
+      if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
+      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 300000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
+      message.channel.send(`:ballot_box_with_check: TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> :stopwatch:  انتهى وقت الروم الصوتي`), 200000))  // 120000 دقيقتان
+    }
+    });
+
 
 
 
@@ -190,9 +314,13 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`System|HAMO`,"http://twitch.tv/S-F")
+client.user.setGame(`ZOMBIE SHOP`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
   
 
 // THIS  MUST  BE  THIS  WAY
